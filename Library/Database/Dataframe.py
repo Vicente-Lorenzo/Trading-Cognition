b43755188dc033cf2da3736f972af073b43dec30
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2026-04-24 19:09:05 +0100

    [Dataclasses] Perfected Dataclasses
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Library/Database/Dataframe.py
+++ Library/Database/Dataframe.py
@@ -70,9 +70,8 @@ class DataframeAPI:
         return None, [[data]], False
 
     def frame(self, data: Any, schema: dict = None, legacy: bool | Missing = MISSING) -> Any:
-        legacy = legacy if legacy is not MISSING else self._legacy_
         data = self.flatten(data)
-        if legacy: return pd.DataFrame(data=data, columns=list(schema.keys()) if schema else None)
         df = pl.DataFrame(data=data, schema=schema, orient="row", strict=False)
         if len(df) > 0: df = df.select([s.shrink_dtype() for s in df.get_columns()])
-        return df
+        legacy = legacy if legacy is not MISSING else self._legacy_
+        return df.to_pandas() if legacy else df
