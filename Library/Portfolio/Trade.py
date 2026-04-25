d9dc0394396ddfe1a74195de8da623ff18dc069e
Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>
2026-04-25 15:08:41 +0100

    [Dataclasses] Perfected Dataclasses
    Signed-off-by: Vicente Lorenzo <vicente.aser.lorenzo@gmail.com>

--- Library/Portfolio/Trade.py
+++ Library/Portfolio/Trade.py
@@ -9,21 +9,21 @@ from Library.Database.Database import IdentityKey, ForeignKey, DatabaseAPI
 from Library.Database.Datapoint import DatapointAPI
 from Library.Database.Dataclass import overridefield
 from Library.Portfolio.Portfolio import PortfolioAPI
-from Library.Portfolio.Position import PositionAPI
+from Library.Portfolio.Position import PositionAPI, PositionType
 from Library.Portfolio.PnL import PnLAPI
 from Library.Market.Timestamp import TimestampAPI
-from Library.Market.Price import PriceAPI
+from Library.Market.Price import PriceAPI, Direction
 from Library.Utility.Typing import MISSING
 
 if TYPE_CHECKING:
     from Library.Universe.Security import SecurityAPI
-    from Library.Universe.Contract import ContractAPI
+    from Library.Portfolio.Order import OrderAPI
 
 @dataclass
 class TradeAPI(PositionAPI):
 
     Database: ClassVar[str] = DatapointAPI.Database
-    Schema: ClassVar[str] = PortfolioAPI.Schema
+    Schema: ClassVar[str] = "Portfolio"
     Table: ClassVar[str] = "Trade"
 
     UID: int | None = None
@@ -38,13 +38,34 @@ class TradeAPI(PositionAPI):
 
     @property
     def Structure(self) -> dict:
-        return {
+        from Library.Universe.Security import SecurityAPI
+        s = super().Structure
+        cols = {
             self.ID.UID: IdentityKey(pl.Int64),
+            self.ID.Security: ForeignKey(pl.Int64, reference=f'"{SecurityAPI.Schema}"."{SecurityAPI.Table}"("{SecurityAPI.ID.UID}")'),
             self.ID.Position: ForeignKey(pl.Int64, reference=f'"{PortfolioAPI.Schema}"."{PositionAPI.Table}"("{PositionAPI.ID.UID}")'),
+            self.ID.Type: pl.Enum([e.name for e in PositionType]),
+            self.ID.Direction: pl.Enum([e.name for e in Direction]),
+            self.ID.Volume: pl.Float64(),
+            self.ID.Quantity: pl.Float64(),
+            self.ID.EntryTimestamp: pl.Datetime(),
             self.ID.ExitTimestamp: pl.Datetime(),
+            self.ID.EntryPrice: pl.Float64(),
+            self.ID.ExitPrice: pl.Float64(),
+            self.ID.GrossPnL: pl.Float64(),
+            self.ID.CommissionPnL: pl.Float64(),
+            self.ID.SwapPnL: pl.Float64(),
+            self.ID.NetPnL: pl.Float64(),
+            self.ID.EntryBalance: pl.Float64(),
+            self.ID.MidBalance: pl.Float64(),
             self.ID.ExitBalance: pl.Float64(),
-            **super().Structure
+            self.ID.Label: pl.String(),
+            self.ID.Comment: pl.String(),
         }
+        for k, v in s.items():
+            if k not in cols:
+                cols[k] = v
+        return cols
 
     def __post_init__(self,
                       db: DatabaseAPI | None,
@@ -52,6 +73,8 @@ class TradeAPI(PositionAPI):
                       autosave: bool,
                       autoload: bool,
                       autooverload: bool,
+                      type: PositionType | str | None,
+                      direction: Direction | str | None,
                       security: int | SecurityAPI | None,
                       entry_timestamp: datetime | TimestampAPI | None,
                       entry_price: float | PriceAPI | None,
@@ -69,7 +92,7 @@ class TradeAPI(PositionAPI):
                       swap_pnl: float | PnLAPI | None,
                       net_pnl: float | PnLAPI | None,
                       entry_balance: float | None,
-                      contract: ContractAPI | None,
+                      order: int | OrderAPI | None,
                       position: int | PositionAPI | None,
                       exit_timestamp: datetime | TimestampAPI | None,
                       exit_balance: float | None) -> None:
@@ -83,17 +106,31 @@ class TradeAPI(PositionAPI):
         if isinstance(exit_timestamp, TimestampAPI): self._exit_timestamp_ = exit_timestamp
         elif exit_timestamp is not MISSING and exit_timestamp is not None:
             self._exit_timestamp_ = TimestampAPI(DateTime=exit_timestamp)
-        super().__post_init__(db=db, migrate=migrate, autosave=autosave, autoload=autoload, autooverload=autooverload,
+        super().__post_init__(db=db, 
+                              migrate=migrate, 
+                              autosave=autosave, 
+                              autoload=autoload, 
+                              autooverload=autooverload,
+                              type=type,
+                              direction=direction,
                               security=security,
-                              entry_timestamp=entry_timestamp, entry_price=entry_price,
-                              stop_loss_price=stop_loss_price, take_profit_price=take_profit_price,
-                              max_run_up_price=max_run_up_price, max_draw_down_price=max_draw_down_price,
+                              entry_timestamp=entry_timestamp, 
+                              entry_price=entry_price,
+                              stop_loss_price=stop_loss_price, 
+                              take_profit_price=take_profit_price,
+                              max_run_up_price=max_run_up_price, 
+                              max_draw_down_price=max_draw_down_price,
                               exit_price=exit_price,
-                              stop_loss_pnl=stop_loss_pnl, take_profit_pnl=take_profit_pnl,
-                              max_run_up_pnl=max_run_up_pnl, max_draw_down_pnl=max_draw_down_pnl,
-                              gross_pnl=gross_pnl, commission_pnl=commission_pnl,
-                              swap_pnl=swap_pnl, net_pnl=net_pnl,
-                              entry_balance=entry_balance, contract=contract)
+                              stop_loss_pnl=stop_loss_pnl, 
+                              take_profit_pnl=take_profit_pnl,
+                              max_run_up_pnl=max_run_up_pnl, 
+                              max_draw_down_pnl=max_draw_down_pnl,
+                              gross_pnl=gross_pnl, 
+                              commission_pnl=commission_pnl,
+                              swap_pnl=swap_pnl, 
+                              net_pnl=net_pnl,
+                              entry_balance=entry_balance, 
+                              order=order)
 
     @property
     @overridefield
